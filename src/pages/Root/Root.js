7f56b8cf9a11d5286dd16aa025bff7c170e9e19f
Ivan Badichan <badican01117@gmail.com>
2021-04-27 21:31:12 +0300

    Styled desktop version

--- src/pages/Root/Root.js
+++ src/pages/Root/Root.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ProjectOverview from './components/ProjectOverview';
 import Comics from './components/Comics';
 import SupportUs from './components/SupportUs';
-// import styles from './style.module.css';
+import styles from './style.module.css';
 
 function RootPage() {
   return (
     <main>
-      <ProjectOverview />
-      <Comics />
-      <SupportUs />
+      <div className={styles.sectionColumns}>
+        <ProjectOverview />
+        <Comics />
+        <SupportUs />
+      </div>
     </main>
   );
 }
